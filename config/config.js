310505ab77eae8d936be34ce2922c13ca286e998
Pradip P Caulagi <caulagi@gmail.com>
2013-09-14 16:57:30 +0530

    Initial project structure based on caulagi/sntd

---
+++ config/config.js
@@ -0,0 +1,38 @@
+
+var rootPath = require('path').normalize(__dirname + '/..')
+  , _ = require('underscore')
+
+var _base = {
+  db: 'mongodb://localhost/toystori',
+  root: rootPath,
+  items_per_page: 30,
+  app: {
+    name: 'toystori: Share your toys'
+  },
+  facebook: {
+    clientID: "1406466249565794",
+    clientSecret: "a2f0757486db1fb8ec396bf16dd86d97",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  fallbackCity: 'Bangalore',
+  fallbackCityId: '51fe014e1f004ba72300522c',
+  MIXPANEL_ID: process.env.MIXPANEL_ID || "86d6a0a2e95c442691e4dc5543dbc833"
+}
+
+var development = _.extend({}, _base, { db: _base.db+'_dev' })
+  , test        = _.extend({}, _base, { db: _base.db+'_test' })
+  , production  = _.extend({}, _base, {
+      db: process.env.MONGOHQ_URL || _base.db+'_prod',
+      facebook: {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "http://toystori.com/auth/facebook/callback"
+      },
+      fallbackCityId: '51fd5227920fc2020000522c'
+    })
+
+module.exports = {
+  development: development,
+  test: test,
+  production: production
+}
---
+++ config/routes.js
@@ -0,0 +1,56 @@
+
+/*!
+ * Module dependencies.
+ */
+
+var async = require('async')
+
+/**
+ * Controllers
+ */
+
+var users = require('../app/controllers/users')
+  , auth = require('./middlewares/authorization')
+
+/**
+ * Expose routes
+ */
+
+module.exports = function (app, passport) {
+
+  // user routes
+  app.get('/login', users.login)
+  app.get('/logout', users.logout)
+  app.get('/users/:userId', users.profile)
+  app.get('/auth/facebook', passport.authenticate('facebook'))
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      failureRedirect: '/login'
+    }), users.authCallback)
+
+  app.param('userId', users.user)
+
+
+  // --- HOME
+  //app.get('/', toys.index)
+
+  // city routes
+  var cities = require('../app/controllers/cities')
+  app.get('/cities/search/:query', cities.search)
+
+  app.param('cityId', cities.load)
+  app.param('query', cities.find)
+
+  app.get('/about', function (req, res) {
+    res.render('about')
+  })
+  app.get('/terms', function (req, res) {
+    res.render('terms')
+  })
+  app.get('/privacy', function (req, res) {
+    res.render('privacy')
+  })
+  app.get('/support', function (req, res) {
+    res.render('support')
+  })
+}
