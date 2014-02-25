8ca0ba87da5698f99fb95026873a9d50fa6ef78d
Pradip P Caulagi <caulagi@gmail.com>
2014-02-25 18:31:00 +0530

    Show toys shared by user on his profile page

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose')
   , User = mongoose.model('User')
   , errors = require('../../lib/errors')
+  , Toy = mongoose.model("Toy")
+
+function renderProfile(res, options) {
+  options["title"] = options.user.name
+  return res.render('users/profile', options)
+}
 
 exports.authCallback = function (req, res, next) {
   res.redirect('/')
@@ -24,9 +30,12 @@ exports.session = function (req, res) {
 
 exports.profile = function (req, res) {
   var user = req.profile
-  res.render('users/profile', {
-    title: user.name,
-    user: user
+  Toy.list({user: user}, function(err, toys) {
+    if (err) return renderProfile(res, {user: user})
+
+    Toy.count().exec(function (err, count) {
+      return renderProfile(res, {user: user, toys: toys})
+    })
   })
 }
 
