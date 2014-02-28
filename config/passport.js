42093b33b9b6ff2bf866c9c504c4485bd8cca9e0
Pradip P Caulagi <caulagi@gmail.com>
2014-02-28 15:21:40 +0530

    Just store the email that comes to use

--- config/passport.js
+++ config/passport.js
@@ -28,10 +28,9 @@ module.exports = function (passport, config) {
       User.findOne({ 'facebook.id': profile.id }, function (err, user) {
         if (err) { return done(err) }
         if (!user) {
-          var email = (profile.emails && profile.emails[0].value) || profile.username+'@invalid.com'
           user = new User({
             name: profile.displayName,
-            email: email,
+            email: profile.emails[0].value,
             username: profile.username,
             provider: 'facebook',
             facebook: profile._json
