81d9111242423b2cf1aa337981369754803c3b1a
Pradip P Caulagi <caulagi@gmail.com>
2014-02-28 15:48:00 +0530

    Login still flaky

--- config/config.js
+++ config/config.js
@@ -29,7 +29,7 @@ var development = _.extend({}, _base, { db: _base.db+'_dev' })
       facebook: {
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "http://www.toystori.com/auth/facebook/callback"
+        callbackURL: "http://fathomless-forest-3647.herokuapp.com/auth/facebook/callback"
       },
       fallbackCityId: '51fd5227920fc2020000522c'
     })
