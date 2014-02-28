395ae0809f8f136a633a0f9ab61d5f6e254a1e24
Pradip P Caulagi <caulagi@gmail.com>
2014-02-28 16:38:56 +0530

    Need to validate the username provided by FB

--- app/models/user.js
+++ app/models/user.js
@@ -2,30 +2,16 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , authTypes = ['twitter']
 
-/**
- * User Schema
- */
-
 var UserSchema = new Schema({
-  name: { type: String, default: '' },
-  email: { type: String, default: '' },
-  username: { type: String, default: '' },
-  provider: { type: String, default: '' },
-  authToken: { type: String, default: '' },
+  name: { type: String },
+  email: { type: String },
+  username: { type: String },
+  provider: { type: String },
+  authToken: { type: String },
   facebook: {},
   verified: { type: Boolean, default: false},
   createdAt: {type : Date, default : Date.now}
 })
-
-var validatePresenceOf = function (value) {
-  return value && value.length
-}
-
-UserSchema.path('username').validate(function (username) {
-  // if you are authenticating by any of the oauth strategies, don't validate
-  if (authTypes.indexOf(this.provider) !== -1) return true
-  return username.length
-}, 'Username cannot be blank')
 
 UserSchema.statics = {
 
